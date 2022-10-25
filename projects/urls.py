8ccebaa64e9f541063d6aa405319f5baee96789b
Isaiah <isababa12@gmail.com>
2022-10-25 11:35:53 -0700

    Feature 14 complete

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import projects_list, show_project
+from .views import projects_list, show_project, create_project
 
 urlpatterns = [
     path("", projects_list, name="list_projects"),
-    path("<int:id>/", show_project, name="show_project")
+    path("<int:id>/", show_project, name="show_project"),
+    path("create/", create_project, name="create_project"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Project
 from django.contrib.auth.decorators import login_required
+from projects.forms import ProjectForm
 
 
 @login_required
@@ -19,3 +20,20 @@ def show_project(request, id):
         "project_details": projects,
     }
     return render(request, "projects/details.html", context)
+
+
+@login_required
+def create_project(request):
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            recipe = form.save(False)
+            recipe.author = request.user
+            recipe.save()
+            return redirect("list_projects")
+    else:
+        form = ProjectForm()
+    context = {
+        "form": form,
+        }
+    return render(request, "projects/create.html", context)
