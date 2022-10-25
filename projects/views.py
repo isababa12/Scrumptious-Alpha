4812a2267624b47c6eb09ff5e34e5f76b1acd587
Isaiah <isababa12@gmail.com>
2022-10-24 20:41:20 -0700

    Feature 10 complete

--- projects/views.py
+++ projects/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def projects_list(request):
-    projects = Project.objects.filter(owner = request.user)
+    projects = Project.objects.filter(owner=request.user)
     context = {
         "projects_list": projects,
     }
